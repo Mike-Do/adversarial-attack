e58fd9c6764da1126384e1e4208b7746ec435dbc
Mike-Do <agentdonoso@gmail.com>
2023-04-29 13:03:55 -0400

    feat: updated

--- main/cnn.py
+++ main/cnn.py
@@ -4,10 +4,55 @@ CNN defined by Carlini and Walker 2016
 import sys
 import pickle as pk
 import tensorflow as tf
+import numpy as np
 from sklearn.datasets import load_digits
 
+class CNN(tf.keras.Model):
+    def __init__(self):
+        """
+        Architecture taken from Carlini and Wagner 2016
+        """
 
-# TODO define class / functions for CNN here
+        """Hyperparameters"""
+        # LearningRate0.1
+        self.learning_rate = 0.1
+        # Momentum0.9
+        self.momentum = 0.9
+        # DelayRate-
+        # Dropout0.5
+        self.dropout = 0.5
+        # BatchSize128
+        self.batch_size = 128
+        # Epochs50
+        self.epochs = 50
+
+        # visualize loss over time
+        self.loss_list = []
+        # momentum-based SGD optimizer
+        self.optimizer = tf.keras.optimizers.SGD(learning_rate=self.learning_rate, momentum=self.momentum)
+
+
+        """Architecture"""
+        # Convolution+ReLU3×3×32
+        self.conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')
+        # Convolution+ReLU3×3×32
+        self.conv2 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')
+        # MaxPooling2×2
+        self.maxpool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))
+        # Convolution+ReLU3×3×64
+        self.conv3 = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu')
+        # Convolution+ReLU3×3×64
+        self.conv4 = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu')
+        # MaxPooling2×2
+        self.maxpool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))
+        # FullyConnected+ReLU200
+        self.fc1 = tf.keras.layers.Dense(units=200, activation='relu')
+        # FullyConnected+ReLU200
+        self.fc2 = tf.keras.layers.Dense(units=200, activation='relu')
+        # Softmax10
+        self.softmax = tf.keras.layers.Dense(units=10, activation='softmax')
+
+
 
 def main(input_filepath="../data/dataset.pk", output_filepath="../data/result.pk"):
 
